1c3661254e9501b13bab9cc9b7af647fbe093c89
John Wheeler <jtlwheeler@gmail.com>
2017-09-06 21:23:21 -0400

    Refactored to use constant variable to represent penny instead of string literal

--- vending_machine.py
+++ vending_machine.py
@@ -1,7 +1,9 @@
+PENNY = "PENNY"
+ 
 class VendingMachine():
     def __init__(self):
         self.coin_return = []
 
     def insert_coin(self, coin):
-        if coin == "PENNY":
+        if coin == PENNY:
             self.coin_return.append(coin)
--- vending_machine_tests.py
+++ vending_machine_tests.py
@@ -1,11 +1,12 @@
 import unittest
+import vending_machine
 from vending_machine import VendingMachine
 
 class VendingMachineTests(unittest.TestCase):
     def test_when_a_penny_is_inserted_it_is_placed_in_the_coin_return(self):
         machine = VendingMachine()
-        machine.insert_coin("PENNY")
-        self.assertListEqual(["PENNY"], machine.coin_return)
+        machine.insert_coin(vending_machine.PENNY)
+        self.assertListEqual([vending_machine.PENNY], machine.coin_return)
 
 if __name__ == '__main__':
     unittest.main()
