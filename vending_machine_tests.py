3117a55efda94a88559e1d1d2fec575ebc2beef5
John Wheeler <jtlwheeler@gmail.com>
2017-09-06 21:26:18 -0400

    Refactored tests to initialize VendingMachine object at start of each test

--- vending_machine_tests.py
+++ vending_machine_tests.py
@@ -3,15 +3,16 @@ import vending_machine
 from vending_machine import VendingMachine
 
 class VendingMachineTests(unittest.TestCase):
+    def setUp(self):
+        self.machine = VendingMachine()
+
     def test_when_a_penny_is_inserted_it_is_placed_in_the_coin_return(self):
-        machine = VendingMachine()
-        machine.insert_coin(vending_machine.PENNY)
-        self.assertListEqual([vending_machine.PENNY], machine.coin_return)
+        self.machine.insert_coin(vending_machine.PENNY)
+        self.assertListEqual([vending_machine.PENNY], self.machine.coin_return)
 
     def test_when_a_nickel_is_inserted_it_is_not_placed_in_the_coin_return(self):
-        machine = VendingMachine()
-        machine.insert_coin(vending_machine.NICKEL)
-        self.assertListEqual([], machine.coin_return)
+        self.machine.insert_coin(vending_machine.NICKEL)
+        self.assertListEqual([], self.machine.coin_return)
 
 if __name__ == '__main__':
     unittest.main()
