6fa47b84646323eb2073a9a41f3db50d3fc4d747
John Wheeler <jtlwheeler@gmail.com>
2017-09-06 21:28:29 -0400

    Refactored to move accepted coins into the inserted coin bin

--- vending_machine.py
+++ vending_machine.py
@@ -4,8 +4,11 @@ DIME = "DIME"
 
 class VendingMachine():
     def __init__(self):
+        self._inserted_coin_bin = {NICKEL : 0, DIME : 0}
         self.coin_return = []
 
     def insert_coin(self, coin):
-        if coin == PENNY:
+        if coin in self._inserted_coin_bin:
+            self._inserted_coin_bin[coin] += 1
+        else:
             self.coin_return.append(coin)
