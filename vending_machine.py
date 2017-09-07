3f97a8ab2f7dac61a3d139abc242d5e2832adbb8
John Wheeler <jtlwheeler@gmail.com>
2017-09-06 21:20:01 -0400

    Initial vending machine commit

---
+++ vending_machine.py
@@ -0,0 +1,7 @@
+class VendingMachine():
+    def __init__(self):
+        self.coin_return = []
+
+    def insert_coin(self, coin):
+        if coin == "PENNY":
+            self.coin_return.append(coin)
