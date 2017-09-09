ab5e0ba8cbfff5953aeb65df0f19b4b2b2e3ecf3
John Wheeler <jtlwheeler@gmail.com>
2017-09-09 19:59:03 -0400

    Tested chips selected with coins and chips in inventory

--- vending_machine_tests.py
+++ vending_machine_tests.py
@@ -332,5 +332,14 @@ class VendingMachineTests(unittest.TestCase):
         self.assertEqual("PRICE $1.00", self.machine.read_display())
         self.assertEqual("INSERT COIN", self.machine.read_display())
 
+    def test_when_machine_has_coins_and_chips_and_no_coins_entered_and_cola_selected_then_display_should_read_price_then_insert_coin(self):
+        self.machine.vendor_load_coin(vm.NICKEL, 10)
+        self.machine.vendor_load_product(vm.CHIPS)
+
+        self.machine.select_product(vm.CHIPS)
+
+        self.assertEqual("PRICE $0.50", self.machine.read_display())
+        self.assertEqual("INSERT COIN", self.machine.read_display())
+
 if __name__ == '__main__':
     unittest.main()
