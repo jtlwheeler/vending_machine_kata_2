8b82e3563d390fc9208e3f96ff1a3b6d53cd7071
John Wheeler <jtlwheeler@gmail.com>
2017-09-10 11:56:52 -0400

    Changed some test case names to follow when-then naming pattern

--- vending_machine_tests.py
+++ vending_machine_tests.py
@@ -6,44 +6,44 @@ class VendingMachineTests(unittest.TestCase):
     def setUp(self):
         self.machine = VendingMachine()
 
-    def test_when_a_penny_is_inserted_it_is_placed_in_the_coin_return(self):
+    def test_when_a_penny_is_inserted_then_it_is_placed_in_the_coin_return(self):
         self.machine.insert_coin(vm.PENNY)
         self.assertDictEqual({vm.PENNY : 1}, self.machine.coin_return)
 
-    def test_when_a_nickel_is_inserted_it_is_not_placed_in_the_coin_return(self):
+    def test_when_a_nickel_is_inserted_then_it_is_not_placed_in_the_coin_return(self):
         self.machine.insert_coin(vm.NICKEL)
         self.assertDictEqual({}, self.machine.coin_return)
 
-    def test_when_a_dime_is_inserted_it_is_not_placed_in_the_coin_return(self):
+    def test_when_a_dime_is_inserted_then_it_is_not_placed_in_the_coin_return(self):
         self.machine.insert_coin(vm.DIME)
         self.assertDictEqual({}, self.machine.coin_return)
 
-    def test_when_a_quarter_is_inserted_it_is_not_placed_in_the_coin_return(self):
+    def test_when_a_quarter_is_inserted_then_it_is_not_placed_in_the_coin_return(self):
         self.machine.insert_coin(vm.QUARTER)
         self.assertDictEqual({}, self.machine.coin_return)
 
-    def test_when_nickel_is_inserted_and_coin_return_button_is_pressed_it_is_placed_in_coin_return(self):
+    def test_when_nickel_is_inserted_and_coin_return_button_is_pressed_then_it_is_placed_in_coin_return(self):
         self.machine.insert_coin(vm.NICKEL)
         self.machine.return_coins()
         self.assertDictEqual({vm.NICKEL : 1}, self.machine.coin_return)
 
-    def test_when_two_nickels_are_inserted_and_coin_return_button_is_pressed_they_are_placed_in_coin_return(self):
+    def test_when_two_nickels_are_inserted_and_coin_return_button_is_pressed_then_they_are_placed_in_coin_return(self):
         self.machine.insert_coin(vm.NICKEL)
         self.machine.insert_coin(vm.NICKEL)
         self.machine.return_coins()
         self.assertDictEqual({vm.NICKEL : 2}, self.machine.coin_return)
 
-    def test_when_dime_is_inserted_and_coin_return_button_is_pressed_it_is_placed_in_coin_return(self):
+    def test_when_dime_is_inserted_and_coin_return_button_is_pressed_then_it_is_placed_in_coin_return(self):
         self.machine.insert_coin(vm.DIME)
         self.machine.return_coins()
         self.assertDictEqual({vm.DIME : 1}, self.machine.coin_return)
 
-    def test_when_quarter_is_inserted_and_coin_return_button_is_pressed_it_is_placed_in_coin_return(self):
+    def test_when_quarter_is_inserted_and_coin_return_button_is_pressed_then_it_is_placed_in_coin_return(self):
         self.machine.insert_coin(vm.QUARTER)
         self.machine.return_coins()
         self.assertDictEqual({vm.QUARTER : 1}, self.machine.coin_return)
 
-    def test_when_coins_are_inserted_and_coin_return_button_is_pressed_they_are_placed_in_coin_return(self):
+    def test_when_coins_are_inserted_and_coin_return_button_is_pressed_then_they_are_placed_in_coin_return(self):
         self.machine.insert_coin(vm.NICKEL)
         self.machine.insert_coin(vm.NICKEL)
         self.machine.insert_coin(vm.PENNY)
@@ -57,15 +57,15 @@ class VendingMachineTests(unittest.TestCase):
         expected_dict = {vm.PENNY : 1, vm.NICKEL : 2, vm.DIME : 2, vm.QUARTER : 2}
         self.assertDictEqual(expected_dict, self.machine.coin_return)
 
-    def test_when_cola_is_selected_without_enough_money_it_is_not_dispensed(self):
+    def test_when_cola_is_selected_without_enough_money_then_it_is_not_dispensed(self):
         self.machine.select_product(vm.COLA)
         self.assertEqual("", self.machine.product_dispense_bin)
 
-    def test_when_vendor_loads_one_cola_it_is_added_to_the_product_inventory(self):
+    def test_when_vendor_loads_one_cola_then_it_is_added_to_the_product_inventory(self):
         self.machine.vendor_load_product(vm.COLA, 1)
         self.assertEqual(1, self.machine.get_product_quantity(vm.COLA))
 
-    def test_when_vendor_loads_five_colas_it_is_added_to_the_product_inventory(self):
+    def test_when_vendor_loads_five_colas_then_the_are__added_to_the_product_inventory(self):
         self.machine.vendor_load_product(vm.COLA, 5)
         self.assertEqual(5, self.machine.get_product_quantity(vm.COLA))
 
